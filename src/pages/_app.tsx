b4539be9858968b0efb8b4eb0f88ffc5a0f3aaeb
João Mello <joao.mellof@furg.br>
2022-04-12 15:05:04 -0300

    fixing type bu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import type { AppProps } from 'next/app'
 
 import { ToastProvider } from '../context/toastContext'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return(
     <ToastProvider>
       <Component {...pageProps} />
