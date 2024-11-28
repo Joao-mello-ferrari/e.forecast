372111346214b8b954ce9924be447d9002b6a1fe
AmandaJMendes <amandajm2305@gmail.com>
2024-11-28 12:00:25 -0300

    including google.news as image domain

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     styledComponents: true,
   },
   images: {
-    domains: ['flagsapi.com', 'openweathermap.org'],
+    domains: ['flagsapi.com', 'openweathermap.org', 'news.google.com'],
     dangerouslyAllowSVG: true,
   },
 }
